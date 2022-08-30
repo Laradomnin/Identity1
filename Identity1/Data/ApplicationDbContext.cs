ea01b32aeb25c55cce90b2c4b1f0a2fef384a265
Laradomnin <lara.domnin@hotmail.com>
2022-08-30 15:22:30 +0200

    Add project files.

---
+++ Identity1/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Identity1.Models.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<Member>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Identity1/Models/Entities/Member.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Identity1.Models.Entities
+{
+    public class Member:IdentityUser
+    {
+        public bool IsPro { get; set; } 
+        public ICollection<Vehicle> Vehicles { get; set; }
+    }
+}
